34474f8c9e54e990eb318ba4b7ebf9a363e8cb66
harshil206 <64388351+harshil206@users.noreply.github.com>
2020-12-05 12:21:44 +0530

    hii

---
+++ Screen/Login.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { } from 'react-native-gesture-handler';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+
+
+import Card from '../Components/Card';
+import Appbutton from '../Components/Appbutton';
+
+const Login = props => {
+  return (
+    <View style={styles.container}>
+
+      <Card style={styles.card}>
+        <View style={styles.headertext}>
+          <Text style={styles.text}>LOGIN</Text>
+          <AntDesign name="login" size={40} color="black" />
+        </View>
+        <View style={styles.textContent}>
+          <Text style={styles.text_style}>Username : </Text>
+          <TextInput style={styles.texteditor_style}></TextInput>
+        </View>
+        <View style={styles.textContent}>
+          <Text style={styles.text_style}>Password : </Text>
+          <TextInput style={styles.texteditor_style} secureTextEntry={true}></TextInput>
+        </View>
+        <Appbutton style={styles.loginbutt}>Login</Appbutton>
+        <TouchableOpacity><Text style={{ fontSize: 15, paddingTop: '4%', fontWeight: 'bold' }}>Forgot Password ??</Text></TouchableOpacity>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  loginbutt: {
+    marginTop: '10%'
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  card: {
+    marginTop: '30%',
+    width: '80%',
+    alignItems: 'center',
+    borderColor: 'black',
+    backgroundColor: '#ccffff'
+  },
+  texteditor_style: {
+    fontSize: 20,
+    width: 140,
+    borderBottomWidth: 2,
+    borderRadius: 10,
+    borderBottomColor: 'black',
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+  headertext: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%'
+  },
+  textContent: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: '10%',
+    width: '100%'
+  },
+  text_style: {
+    fontSize: 20
+  }
+});
+
+export default Login;
